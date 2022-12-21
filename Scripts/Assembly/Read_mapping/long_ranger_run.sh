7ee124206106ea2a5705261e8b3aebae2470d574
Lars Höök <lars.hook@ebc.uu.se>
2022-12-21 18:20:13 +0100

    Create long_ranger_run.sh

---
+++ Scripts/Assembly/Read_mapping/long_ranger_run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH -J LongRanger
+#SBATCH -p node
+#SBATCH -n 1
+#SBATCH -t 10:00:00
+#SBATCH -A snic2022-5-34
+
+# load modules
+module load bioinfo-tools
+module load longranger/2.2.2
+
+
+# process 10X reads, remove barcodes etc.
+
+
+ASSEMBLY=P14502_103
+
+cp /proj/uppstore2017185/b2014034/private/raw_data/Genome_assembly_data/N.Backstrom_19_10/$ASSEMBLY/02-FASTQ/191111_A00621_0145_AHTTMKDSXX/* $SNIC_TMP
+
+cd $SNIC_TMP
+
+longranger basic \
+        --id=$ASSEMBLY \
+        --fastqs=$SNIC_TMP
+
+cp -r $SNIC_TMP/$ASSEMBLY/ /proj/uppstore2017185/b2014034/private/raw_data/Genome_assembly_data/N.Backstrom_19_10/$ASSEMBLY/
+
+
+#/home/larshook/LarsH/LongRanger_basic

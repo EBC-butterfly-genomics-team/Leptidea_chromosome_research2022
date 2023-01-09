a823e08dec38bd6a5405f64bcef5aa8884611f04
Lars Höök <lars.hook@ebc.uu.se>
2023-01-09 16:24:55 +0100

    Create reformat_gff_forMCScanX.sh

---
+++ Scripts/Synteny/reformat_gff_forMCScanX.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# change format of maker.gff to format suitable as input for MCScanx
+# run ./reformat_gff_for_MCScanx.sh <maker.gff> <sp. abbreviation>
+# sp. abbreviation is a two letter short for the species used
+
+
+# set number of chromosomes in for loop...
+
+for i in {1..42};
+do
+ grep -w "HiC_scaffold_"$i"" "$1";
+done |\
+
+awk '{if ($3=="gene") print $1, $9, $4, $5}' |\
+         sed "s/HiC_scaffold_/$2/; s/ID=//;s/;/ /; s/G/T/" |\
+         awk -v OFS="\t" '{print $1, $2, $4, $5}' > "$2".gff

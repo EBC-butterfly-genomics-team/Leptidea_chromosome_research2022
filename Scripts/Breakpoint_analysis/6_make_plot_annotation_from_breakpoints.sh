707619bd3ab8c3b49b1a77b6026710821417a17e
Lars Höök <lars.hook@ebc.uu.se>
2023-04-06 14:15:11 +0200

    Update 6_make_plot_annotation_from_breakpoints.sh

--- Scripts/Breakpoint_analysis/6_make_plot_annotation_from_breakpoints.sh
+++ Scripts/Breakpoint_analysis/6_make_plot_annotation_from_breakpoints.sh
@@ -5,6 +5,8 @@
 
 # run: ./make_plot_annotation_from_breakpoints.sh <breakpoint_coordinates.file>
 
+cd /home/larshook/LarsH/WHOLE_GENOME_ALIGNMENTS/BREAKPOINT_ANALYSIS
+
 printf "chr fi_swe fi_spa fi_rea fi_anc_sin fu_swe fu_spa fu_rea fu_anc_sin fi_juv_spa\n" > "$1"_plot_annotation
 
 awk '{if ($4=="fi_swe") print $1, $2, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA""\n"$1, $3, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA"; \

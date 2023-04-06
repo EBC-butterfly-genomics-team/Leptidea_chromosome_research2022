640030118ac8b31634a7888895a2a1b135575a49
Lars Höök <lars.hook@ebc.uu.se>
2023-04-06 14:06:08 +0200

    Create 5_count_rearrangements.sh

---
+++ Scripts/Breakpoint_analysis/5_count_rearrangements.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+printf "Rearrangements in "$1"\n\n" > "$1".count
+
+printf "fi_swe: " >> "$1".count
+grep "fi_swe" "$1" | wc -l >> "$1".count
+
+printf "fi_spa: " >> "$1".count
+grep "fi_spa" "$1" | wc -l >> "$1".count
+
+printf "fi_rea: " >> "$1".count
+grep "fi_rea" "$1" | wc -l >> "$1".count
+
+printf "fi_anc_sin: " >> "$1".count
+grep "fi_anc_sin" "$1" | wc -l >> "$1".count
+
+printf "fu_swe: " >> "$1".count
+grep "fu_swe" "$1" | wc -l >> "$1".count
+
+printf "fu_spa: " >> "$1".count
+grep "fu_spa" "$1" | wc -l >> "$1".count
+
+printf "fu_rea: " >> "$1".count
+grep "fu_rea" "$1" | wc -l >> "$1".count
+
+printf "fu_anc_sin: " >> "$1".count
+grep "fu_anc_sin" "$1" | wc -l >> "$1".count
+
+printf "BR/ILS_juv/spa: " >> "$1".count
+grep "BR/ILS_juv/spa" "$1" | wc -l >> "$1".count
+
+printf "BR/ILS_juv/swe: " >> "$1".count
+grep "BR/ILS_juv/swe" "$1" | wc -l >> "$1".count
+
+printf "BR/ILS_rea/spa: " >> "$1".count
+grep "BR/ILS_rea/spa" "$1" | wc -l >> "$1".count
+
+printf "BR/ILS_rea/swe: " >> "$1".count
+grep "BR/ILS_rea/swe" "$1" | wc -l >> "$1".count
+
+printf "fi_juv/fu_anc_rea/sin: " >> "$1".count
+grep "fi_juv/fu_anc_rea/sin" "$1" | wc -l >> "$1".count 
+
+printf "fu_juv/fi_anc_rea/sin: " >> "$1".count
+grep "fu_juv/fi_anc_rea/sin" "$1" | wc -l >> "$1".count
